0a37234dbebe4d0f8f7bc27bb22c9b9abd4b4ae4
AmaiOmikan <64887878+kg9n3n8y@users.noreply.github.com>
2025-04-27 20:21:45 +0900

    Add files via upload

---
+++ fudalist.js
@@ -0,0 +1,17 @@
+const fudalist = [
+    // う
+    { no:  65, kimariji: "うら", normal: "./torifuda/tori_ura.png", reverse: "./torifuda/tori_r_ura.png"},
+    { no:  74, kimariji: "うか", normal: "./torifuda/tori_uka.png", reverse: "./torifuda/tori_r_uka.png"},
+    // つ
+    { no:  13, kimariji: "つく", normal: "./torifuda/tori_tsuku.png", reverse: "./torifuda/tori_r_tsuku.png"},
+    { no:  23, kimariji: "つき", normal: "./torifuda/tori_tsuki.png", reverse: "./torifuda/tori_r_tsuki.png"},
+    // し
+    { no:  37, kimariji: "しら", normal: "./torifuda/tori_shira.png", reverse: "./torifuda/tori_r_shira.png"},
+    { no:  40, kimariji: "しの", normal: "./torifuda/tori_shino.png", reverse: "./torifuda/tori_r_shino.png"},
+    // も
+    { no:  66, kimariji: "もろ", normal: "./torifuda/tori_moro.png", reverse: "./torifuda/tori_r_moro.png"},
+    { no: 100, kimariji: "もも", normal: "./torifuda/tori_momo.png", reverse: "./torifuda/tori_r_momo.png"},
+    // ゆ
+    { no:  46, kimariji: "ゆら", normal: "./torifuda/tori_yura.png", reverse: "./torifuda/tori_r_yura.png"},
+    { no:  71, kimariji: "ゆう", normal: "./torifuda/tori_yuu.png", reverse: "./torifuda/tori_r_yuu.png"}
+]
